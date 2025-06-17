85acece7930fbc9762355428f2db4b6573612682
Thanwa14 <puttazritanwa@gmail.com>
2025-06-17 14:20:29 +0700

    firstcommit

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/todos")
+async def get_todos():
+    return [
+         {"id": 1, "detail": "first todos"},
+         {"id": 2, "detail": "second todos"}
+    ]
+counter = 0
+@app.get("/counter")
+async def get_counter():
+    global counter
+    counter += 1
+    return {"message" : f"counter = {counter}"}
